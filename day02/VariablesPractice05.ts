12f1858d9cbeaed340fb9b9e0816a9ca42d2d24e
ShreyB <shreypractice.github@gmail.com>
2026-05-03 23:06:42 +0530

    Updates_0305_23:07

---
+++ day02/VariablesPractice05.ts
@@ -0,0 +1,9 @@
+function constMethod01()
+{
+    if(true)
+    {
+        const msg05="This is a function which uses const keyword and is defined inside a block";
+        console.log(msg05);
+    }
+}
+constMethod01();
