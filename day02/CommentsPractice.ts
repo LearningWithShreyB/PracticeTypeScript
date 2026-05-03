47b463061cee978ef15832a971134c8f1680b565
ShreyB <shreypractice.github@gmail.com>
2026-05-03 22:49:01 +0530

    Updates_0305_22:49

---
+++ day02/CommentsPractice.ts
@@ -0,0 +1,6 @@
+//In order to do the single line comment code, use // 
+// Also if you want to do with keyboard then use ctrl+/
+
+/* In order to do the multi line comment code then use (/*-----*/
+/* Also if you want to do with the help of Keyboard
+then use Shift+Alt+A */
