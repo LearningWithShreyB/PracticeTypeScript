1b15c621110f0c6c80305da50732bf4cc38453f7
ShreyB <shreypractice.github@gmail.com>
2026-05-03 22:56:43 +0530

    Updates_0305_22:57

---
+++ day02/VariablesPractice02.ts
@@ -0,0 +1,10 @@
+function varMethod02()
+{
+    if(true)
+    {
+        var msg02="This is a method function which uses var keyword for functional scope";
+        //console.log(msg01);
+    }
+    console.log(msg02);
+}
+varMethod02();
