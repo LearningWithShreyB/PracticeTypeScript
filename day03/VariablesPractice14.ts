03dec161850b9a0db5ec08b5f0fa91dec422934a
ShreyB <shreypractice.github@gmail.com>
2026-05-04 16:04:50 +0530

    Updates_0405_16:04

---
+++ day03/VariablesPractice14.ts
@@ -0,0 +1,7 @@
+console.log("Practicing a program by Re-initialization through let keyword.");
+let e="Practicing Typescript";
+console.log(e);
+e="Practicing Typescript for Playwright!!";
+console.log(e);
+// /* //e=200;      Incorrect
+// console.log(b); */
---
+++ day03/VariablesPractice15.ts
@@ -0,0 +1,5 @@
+console.log("Practicing a program by Re-declaring through const keyword.");
+/* const c=100;    
+console.log(c); */
+const c="Practicing Typescript!!";
+console.log(c);
