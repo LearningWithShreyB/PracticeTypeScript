e34c4431860514e2bd218bd7e438438b6d140029
ShreyB <shreypractice.github@gmail.com>
2026-05-04 23:16:16 +0530

    Updates_0405_23:17

--- day04/AnyDataTypePractice01.ts
+++ day04/AnyDataTypePractice01.ts
@@ -2,7 +2,7 @@ export{}
 
 console.log("----Understanding Any data type in TypeScript----");
 
-console.log("Program where nothing is defined");
+console.log("Program where nothing is defined in start");
 let value:any;
 value="Hello";
 console.log("The value is:", value);
@@ -11,3 +11,12 @@ console.log("The data type value is:", typeof(value));
 value=101;
 console.log("The value is:", value);
 console.log("The data type value is:", typeof(value));
+
+console.log("Program where some value is defined");
+let value_1:any=true;
+console.log("The value is:", value_1);
+console.log("The data type value is:", typeof(value_1));
+
+value_1=null;
+console.log("The value is:", value_1);
+console.log("The data type value is:", typeof(value_1));
