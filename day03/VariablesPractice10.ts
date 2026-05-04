e4c5aaed7cc571a0e019b732374cfe5a9cc40d40
ShreyB <shreypractice.github@gmail.com>
2026-05-04 15:55:53 +0530

    Updates_0405_15:56

--- day03/VariablesPractice10.ts
+++ day03/VariablesPractice10.ts
@@ -1,5 +1,5 @@
 console.log("Practicing a program by Re-declaring through var keyword.");
-var x=30;
-console.log(x);
-var x="Practicing Typescript!!";
-console.log(x);
+var a=30;
+console.log(a);
+var a="Practicing Typescript!!";
+console.log(a);
---
+++ day03/VariablesPractice11.ts
@@ -0,0 +1,5 @@
+console.log("Practicing a program by Re-declaring through let keyword.");
+let b=50;
+/* console.log(b);
+let b="Practicing Typescript!!"; */
+console.log(b);
