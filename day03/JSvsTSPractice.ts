6d60da7e957a409e9bc56474eb6fcbeb2f8b4d26
ShreyB <shreypractice.github@gmail.com>
2026-05-04 16:58:03 +0530

    Updates_0405_16:58

--- day03/JSvsTSPractice.ts
+++ day03/JSvsTSPractice.ts
@@ -7,3 +7,16 @@ function add(a:number,b:number): number
 let result = add(50,50);
 console.log("Result: "+result);
 
+
+console.log("Creating a function for adding two different datatypes")
+
+function add01(c:number,d:number): number
+{
+    return c+d;
+}
+// Wrong usage (TypeScript error)
+let result01 = add01(50, "50"); // ❌ Compile-time error
+console.log("Result: "+result01);
+
+
+
