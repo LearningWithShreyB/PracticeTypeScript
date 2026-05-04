053da07b63891a20db7092a41f527e7e1d015b43
ShreyB <shreypractice.github@gmail.com>
2026-05-04 16:06:35 +0530

    Updates_0405_16:07

--- day03/VariablesPractice15.ts
+++ day03/VariablesPractice15.ts
@@ -1,5 +1,5 @@
-console.log("Practicing a program by Re-declaring through const keyword.");
-/* const c=100;    
-console.log(c); */
-const c="Practicing Typescript!!";
+console.log("Practicing a program by Re-initialization through const keyword.");
+const c=100;    
 console.log(c);
+/* c=1200;
+console.log(c); */
