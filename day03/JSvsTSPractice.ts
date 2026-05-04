dbeebc527d066d89cfa68b0d282413eff01a0e7f
ShreyB <shreypractice.github@gmail.com>
2026-05-04 17:03:43 +0530

    Updates_0405_17:04

--- day03/JSvsTSPractice.ts
+++ day03/JSvsTSPractice.ts
@@ -19,4 +19,15 @@ let result01 = add01(50, "50"); // ❌ Compile-time error
 console.log("Result: "+result01);
 
 
+console.log("Creating a function for adding two different datatypes")
+
+function add02(e,f)
+{
+    return e+f;
+}
+
+let result02 = add02(10, "1");
+console.log("Result: "+result02);
+
+
 
