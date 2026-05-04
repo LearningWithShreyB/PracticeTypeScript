9328c5d16500869b402f047f4004da8ed83f744d
ShreyB <shreypractice.github@gmail.com>
2026-05-04 22:02:54 +0530

    Updates_0405_22:03

---
+++ day04/StringDataTypePractice03.ts
@@ -0,0 +1,10 @@
+export{}
+
+let name:string="Shrey";
+console.log("The first name is:",name);
+
+let age:number=27;
+console.log("The age is:",age);
+
+const greeting:string=`Hello, my name is ${name} and current age is ${age}`;
+console.log("The result is:",greeting);
