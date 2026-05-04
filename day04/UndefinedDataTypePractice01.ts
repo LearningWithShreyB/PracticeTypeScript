723ed8bb2eef5ca1635de492a54b47919b82c065
ShreyB <shreypractice.github@gmail.com>
2026-05-04 23:02:07 +0530

    Updates_0405_23:03

---
+++ day04/UndefinedDataTypePractice01.ts
@@ -0,0 +1,13 @@
+export{}
+
+console.log("----Understanding undefined data type in TypeScript----");
+
+console.log("Program where nothing is defined");
+let value;
+console.log("The value is:", value);
+console.log("The data type value is:", typeof(value));
+
+console.log("Program where we write undefined datatype");
+let value_1:undefined;
+console.log("The value is:", value_1);
+console.log("The data type value is:", typeof(value_1));
