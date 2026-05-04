9f18652ef4ea0ee57d2c05101447a7ceb5fede25
ShreyB <shreypractice.github@gmail.com>
2026-05-04 18:34:22 +0530

    Updates_0405_18:35

---
+++ day04/StringDataTypePractice01.ts
@@ -0,0 +1,9 @@
+export{}
+
+let first_name:string="Shrey";
+console.log("The first name is :",first_name);
+
+let last_name='B';
+console.log("The last name is :",last_name);
+
+console.log("Hello",first_name,last_name);
