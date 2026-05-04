88608b5d3abcfe827605312f867be458d3a90e6c
ShreyB <shreypractice.github@gmail.com>
2026-05-04 16:01:34 +0530

    Updates_0405_16:02

---
+++ day03/VariablesPractice13.ts
@@ -0,0 +1,7 @@
+console.log("Practicing a program by Re-initialization through var keyword.");
+var d=30;
+console.log(d);
+d=100;
+console.log(d);
+d="Practicing Typescript"  //Avoidable
+console.log(d);
