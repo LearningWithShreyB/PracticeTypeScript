4a58421225fc283a337579a6fd10fb341f9c3158
ShreyB <shreypractice.github@gmail.com>
2026-05-04 18:27:35 +0530

    Updates_0405_18:28

---
+++ day04/NumberDataTypePractice02.ts
@@ -0,0 +1,13 @@
+export{}
+
+let a:number=100.12323;
+console.log("The first number is:",a);
+
+let b:number=200;
+console.log("The second number is:",b);
+
+let c:number=0.2523232;
+console.log("The third number is:",c);
+
+const addResult:number=a+b+c;
+console.log("The result is:", addResult);
