a0c908f2c7c25856b9b1406c241c6010ec46c215
ShreyB <shreypractice.github@gmail.com>
2026-05-04 22:56:32 +0530

    Updates_0405_22:57

---
+++ day04/NullDataTypePractice01.ts
@@ -0,0 +1,7 @@
+export{}
+
+console.log("Writing a program for using null datatype in TypeScript");
+
+let null_value:null=null;
+console.log("The value is:", null_value);
+console.log("The data type value is:", typeof(null_value));
