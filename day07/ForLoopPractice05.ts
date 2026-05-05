3efad5c78d9382ac01156d7524c89aff678e1477
ShreyB <shreypractice.github@gmail.com>
2026-05-05 23:57:49 +0530

    Updates_0505_23:58

---
+++ day07/ForLoopPractice05.ts
@@ -0,0 +1,14 @@
+export{}
+
+console.log("**** Practicing Do-While loop ****");
+
+let i:number;
+
+console.log("\nThe set of numbers are: ");
+
+for(let i:number=1;i<=50;i+=2)
+{
+    console.log(i);
+}
+
+console.log(i);
