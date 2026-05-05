8e5e4867f18d9155f3eef76bec6ff0168e673096
ShreyB <shreypractice.github@gmail.com>
2026-05-05 11:37:10 +0530

    Updates_0505_11:38

---
+++ day05/RelationalOperatorsPractice.ts
@@ -0,0 +1,19 @@
+export {}
+
+console.log("----- OPERATORS IN TYPESCRIPT -----");
+
+let num1: number = 10;
+let num2: number = 20;
+
+console.log("The value of num1 is:",num1);
+console.log("The value of num2 is:",num2);
+
+console.log("\n--- Relational Operators ---");
+
+console.log("num1 > num2:", num1 > num2);
+console.log("num1 < num2:", num1 < num2);
+console.log("num1 >= num2:", num1 >= num2);
+console.log("num1 <= num2:", num1 <= num2);
+console.log("num1 == num2:", num1 == num2);
+console.log("num1 != num2:", num1 != num2);
+console.log("num1 === num2:", num1 === num2);
