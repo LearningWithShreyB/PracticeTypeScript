3e02f69b81be5d5ed69cfc1068ae2f3281bc2b8e
ShreyB <shreypractice.github@gmail.com>
2026-05-05 11:43:47 +0530

    Updates_0505_11:44

---
+++ day05/EqualityVSStrictEqualityOperatorsPractice.ts
@@ -0,0 +1,15 @@
+export { }
+
+console.log("----- OPERATORS IN TYPESCRIPT -----");
+
+let num1: any = 10;
+let num2: any = "10";
+
+console.log("The value of num1 is:", num1);
+console.log("The value of num2 is:", num2);
+
+console.log("\n--- Difference between Equality and Strict Equality Operators ---");
+
+console.log("num1 == num2:", num1 == num2);
+
+console.log("num1 === num2:", num1 === num2);
