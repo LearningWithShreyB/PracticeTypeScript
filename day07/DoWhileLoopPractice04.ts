80cbb2f059d1239d2f99e0ff9266fb0bacbfb6ec
ShreyB <shreypractice.github@gmail.com>
2026-05-05 23:45:10 +0530

    Updates_0505_23:46

---
+++ day07/DoWhileLoopPractice04.ts
@@ -0,0 +1,12 @@
+export { }
+console.log("**** Practicing Do-While loop ****");
+let i: number = 401;
+console.log("\nThe set of numbers are: ");
+do {
+    if(i%2!=0)
+    {
+        console.log(i);
+    }
+
+    i++;
+} while (i <= 220);
