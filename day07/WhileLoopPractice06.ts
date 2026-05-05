584e72169e8aa3718c8980b09c933304bd518fee
ShreyB <shreypractice.github@gmail.com>
2026-05-05 23:35:51 +0530

    Updates_0505_23:36

---
+++ day07/WhileLoopPractice06.ts
@@ -0,0 +1,13 @@
+export { }
+
+console.log("**** Practicing While loop ****");
+
+let i: number = 10000;
+
+console.log("The set of numbers are: ");
+while (i <= 10050) {
+    if (i % 2 === 0) { 
+        console.log(i); }
+
+    i++;
+}
