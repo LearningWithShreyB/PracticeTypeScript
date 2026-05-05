fbaad0b7df794dfbe23d30b4d3e538b9506058e9
ShreyB <shreypractice.github@gmail.com>
2026-05-06 00:01:18 +0530

    Updates_0605_00:02

---
+++ day07/BreakPractice01.ts
@@ -0,0 +1,14 @@
+export{}
+
+console.log("**** Practicing break command ****");
+
+console.log("\nThe set of numbers are: ");
+
+for(let i:number=1;i<=120;i++)
+{
+    if(i===7)
+    {
+       break; 
+    }
+    console.log(i);
+}
