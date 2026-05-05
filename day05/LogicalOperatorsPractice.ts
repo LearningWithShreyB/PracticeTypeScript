4d6ebf5cb1a67f2c6970fe0768d2f0b94941f7e0
ShreyB <shreypractice.github@gmail.com>
2026-05-05 11:51:04 +0530

    Updates_0505_11:52

---
+++ day05/LogicalOperatorsPractice.ts
@@ -0,0 +1,14 @@
+export {}
+
+console.log("----- OPERATORS IN TYPESCRIPT -----");
+
+let isAdult: boolean = true;
+let hasID: boolean = false;
+
+console.log("The value of isAdult is:",isAdult);
+console.log("The value of hasID is:",hasID);
+
+console.log("\n--- Logical Operators ---");
+console.log("isAdult AND hasID:", isAdult && hasID);
+console.log("isAdult OR hasID:", isAdult || hasID);
+console.log("NOT isAdult:", !isAdult);
