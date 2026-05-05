2c7cba552533e7bba27682689638557339576839
ShreyB <shreypractice.github@gmail.com>
2026-05-05 11:11:04 +0530

    Updates_0505_11:11

---
+++ day05/ArithmeticOperatorsPractice.ts
@@ -0,0 +1,14 @@
+export {}
+
+console.log("----- OPERATORS IN TYPESCRIPT -----");
+
+let a: number = 10;
+let b: number = 5;
+
+console.log("\n--- Arithmetic Operators ---");
+
+console.log("Addition:",a + b);
+console.log("Subtraction:", a - b);     
+console.log("Multiplication:", a * b);  
+console.log("Division:", a / b);        
+console.log("Modulus:", a % b);         
