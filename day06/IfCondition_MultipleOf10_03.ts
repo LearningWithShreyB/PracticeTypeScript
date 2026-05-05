918f38cc5a3919ce6d06d4e8ae239ce953fe31d3
ShreyB <shreypractice.github@gmail.com>
2026-05-05 17:10:53 +0530

    Updates_0505_17:11

---
+++ day06/IfCondition_MultipleOf10_03.ts
@@ -0,0 +1,10 @@
+export { }
+
+console.log("**** Practicing IF Condition to check multiple of 10 ****");
+
+let num01: number = 41;
+console.log('The given number is:', num01);
+
+if ((num01 % 10) === 0) {
+    console.log(`The given number ${num01} is a multiple of 10`);
+}
