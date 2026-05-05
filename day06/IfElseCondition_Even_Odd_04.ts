91480ac60ddf7e158efeec996cddb9d6b8c7fc4a
ShreyB <shreypractice.github@gmail.com>
2026-05-05 17:19:06 +0530

    Updates_0505_17:20

---
+++ day06/IfElseCondition_Even_Odd_04.ts
@@ -0,0 +1,15 @@
+export {}
+
+console.log("**** Practicing IF-ELSE Condition to check number is odd or even ****");
+
+let num1: number = 700;
+console.log('The given number is: '+ num1);
+
+if(num1%2===0)
+{
+    console.log(`${num1} is an even number`);
+}
+else
+{
+    console.log(`${num1} is an odd number`);
+}
