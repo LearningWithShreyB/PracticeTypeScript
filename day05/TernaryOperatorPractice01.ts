ca8f13893b2ba38ba23dad8bb071372f3e6bc9e4
ShreyB <shreypractice.github@gmail.com>
2026-05-05 12:55:02 +0530

    Updates_0505_12:55

--- day05/TernaryOperatorPractice01.ts
+++ day05/TernaryOperatorPractice01.ts
@@ -2,6 +2,9 @@ export {}
 
 console.log("----- OPERATORS IN TYPESCRIPT -----");
 
+console.log("\n--- Ternary Operators ---");
+
+console.log("\n--- First Program ---");
 let num1: number = 10;
 let num2: number = 20;
 
@@ -10,6 +13,8 @@ console.log("The value of num2 is:",num2);
 
 let res:string=(num1>num2)?"Number 1 is greatest":"Number 2 is greatest";
 console.log(res);
+
+console.log("\n--- Second Program ---");
 
 num1 = 100;
 num2 = 20;
---
+++ day05/TernaryOperatorPractice02.ts
@@ -0,0 +1,30 @@
+export {}
+
+console.log("----- OPERATORS IN TYPESCRIPT -----");
+
+console.log("\n--- Ternary Operators ---");
+
+console.log("\n--- First Program where age is 18 ---");
+let age01: number = 18;
+
+console.log("The value of age is:",age01);
+
+let res01:string=(age01>=18)?"Eligible to vote":"Not eligible";
+console.log(res01);
+
+console.log("\n--- Second Program where age is greater than 18 ---");
+let age02: number = 27;
+
+console.log("The value of age is:",age02);
+
+let res02:string=(age02>=18)?"Eligible to vote":"Not eligible";
+console.log(res02);
+
+console.log("\n--- Third Program where age is less than 18 ---");
+let age03: number = 7;
+
+console.log("The value of age is:",age03);
+
+let res03:string=(age03>=18)?"Eligible to vote":"Not eligible";
+console.log(res03);
+
