ba1e23caaa0cc284936460dd2e087feec8442409
ShreyB <shreypractice.github@gmail.com>
2026-05-05 17:25:15 +0530

    Updates_0505_17:26

---
+++ day06/IfElseConditionPractice02.ts
@@ -0,0 +1,13 @@
+export { }
+
+console.log("**** Practicing IF-ELSE Condition ****");
+
+let num1: number = 5;
+console.log('The given number is: ' + num1);
+
+if (num1 >= 13 && num1 <= 19) {
+    console.log(`The person is a teenager whose age is ${num1}.`);
+}
+else {
+    console.log(`The person is not a teenager whose age is ${num1}.`);
+}
