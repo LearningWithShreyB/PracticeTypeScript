103b2cb6125291676a71596146077947b16e20ee
ShreyB <shreypractice.github@gmail.com>
2026-05-05 17:04:29 +0530

    Updates_0505_17:05

---
+++ day06/IfCondition_Uppercase02.ts
@@ -0,0 +1,10 @@
+export { }
+
+console.log("**** Practicing IF Condition to check whether a character is uppercase or not ****");
+
+let char01: string = 'K';
+console.log("The given character is:", char01);
+
+if (char01 >= 'A' && char01 <= 'Z') {
+    console.log(`The given character ${char01} is in uppercase.`);
+}
