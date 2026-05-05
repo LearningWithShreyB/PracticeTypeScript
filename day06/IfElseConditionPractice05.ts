b5c52ae65e26df246da10c1c8a3a4909979e3583
ShreyB <shreypractice.github@gmail.com>
2026-05-05 17:51:48 +0530

    Updates_0505_17:52

--- day06/IfElseConditionPractice05.ts
+++ day06/IfElseConditionPractice05.ts
@@ -2,13 +2,12 @@ export { }
 
 console.log("**** Practicing IF-ELSE Condition ****");
 
-let num1: number = 543, num2: number = 213;
-console.log('The first number is: ' + num1);
-console.log(`The second number is: ${num2}`);
+let num1: number = 10;
+console.log('The given number is: ' + num1);
 
-if (num1 > num2) {
-    console.log(`The first number is largest whose value is ${num1}.`);
+if (num1 >=60) {
+    console.log(`You are eligible for senior citizen discounts.`);
 }
 else {
-    console.log(`The second number is largest whose value is ${num2}.`);
+    console.log(`You are not eligible for senior citizen discounts.`);
 }
