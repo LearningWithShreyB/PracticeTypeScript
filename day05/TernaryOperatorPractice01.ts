d62d202228bac304a997ed6b50eb7327ba99cf71
ShreyB <shreypractice.github@gmail.com>
2026-05-05 12:46:33 +0530

    Updates_0505_12:47

---
+++ day05/TernaryOperatorPractice01.ts
@@ -0,0 +1,21 @@
+export {}
+
+console.log("----- OPERATORS IN TYPESCRIPT -----");
+
+let num1: number = 10;
+let num2: number = 20;
+
+console.log("The value of num1 is:",num1);
+console.log("The value of num2 is:",num2);
+
+let res:string=(num1>num2)?"Number 1 is greatest":"Number 2 is greatest";
+console.log(res);
+
+num1 = 100;
+num2 = 20;
+
+console.log("The value of num1 is:",num1);
+console.log("The value of num2 is:",num2);
+
+let res01:string=(num1>num2)?"Number 1 is greatest":"Number 2 is greatest";
+console.log(res01);
