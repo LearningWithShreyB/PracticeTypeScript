d6112d26c4786eb31fe7b38fa41cb0f13a458691
ShreyB <shreypractice.github@gmail.com>
2026-05-05 16:51:23 +0530

    Updates_0505_16:52

---
+++ day06/IfConditionPractice01.ts
@@ -0,0 +1,20 @@
+export{}
+
+console.log("**** Practicing IF Condition ****");
+
+let height:number=5.75;
+console.log(`The height of the person is ${height}`);
+
+if(height>=5.50)
+{
+    console.log("You are eligible for NDA exam.");
+}
+
+
+height=5;
+console.log(`The height of the person is ${height}`);
+
+if(height<=5.50)
+{
+    console.log("You are not eligible for NDA exam.");
+}
