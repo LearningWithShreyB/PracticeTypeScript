9225165667508273ad3f1051f8ac13226a9aa450
ShreyB <shreypractice.github@gmail.com>
2026-05-06 10:52:15 +0530

    Updates_0605_10:53

---
+++ day07/WhileLoopPractice07.ts
@@ -0,0 +1,15 @@
+export{}
+
+console.log("**** Practicing While loop ****");
+
+console.log("\n**** Sum of first 10 natural numbers ****");
+
+let i:number=1;
+let sum:number=0;
+console.log("The result is: ");
+while(i<=10)
+{
+    sum=sum+i;
+    i++;
+}
+console.log(sum);
