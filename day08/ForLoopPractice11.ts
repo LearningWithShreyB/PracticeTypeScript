94391dcdf52ebd3195f20535c85b504d996c70eb
ShreyB <shreypractice.github@gmail.com>
2026-05-09 23:58:00 +0530

    Updates_0905_23:58

---
+++ day08/ForLoopPractice11.ts
@@ -0,0 +1,12 @@
+export { }
+console.log("**** Practicing For loop ****");
+
+console.log("\n**** Print Table of 7 ****");
+
+console.log("\nThe result is: ");
+
+for (let i: number = 7; i <= 70; i += 7) {
+
+    console.log(i);
+}
+
