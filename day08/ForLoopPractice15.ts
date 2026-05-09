2a68d0841f1cba798617afe70e0427c88ea7d590
ShreyB <shreypractice.github@gmail.com>
2026-05-10 00:32:53 +0530

    Updates_1005_00:33

---
+++ day08/ForLoopPractice15.ts
@@ -0,0 +1,12 @@
+export { }
+console.log("**** Practicing For loop ****");
+
+console.log("\n**** Print Multiples of 7 between 1 and 100 ****");
+
+console.log("\nThe result is: ");
+
+for (let i: number = 1; i < 100; i++) {
+    if (i % 7 === 0) {
+        console.log(i);
+    }
+}
