37c04a41ee3be6b71d2713b748042d1d4fade714
ShreyB <shreypractice.github@gmail.com>
2026-05-09 23:42:15 +0530

    Updates_0905_23:43

---
+++ day08/ForLoopPractice08.ts
@@ -0,0 +1,24 @@
+export { }
+console.log("**** Practicing For loop ****");
+
+console.log("\n**** Print Prime Numbers between 1 and 50 ****");
+
+let num: number = 500;
+
+for (let i: number = 1; i <= num; i++) {
+let isPrime: boolean = true;
+
+    if (i < 2) {
+        isPrime = false;
+    }
+
+    for (let j: number = 2; j <= Math.sqrt(i); j++) {
+        if (i % j === 0) {
+            isPrime = false;
+            break;
+        }
+    }
+    if (isPrime) {
+        console.log(i);
+    }
+}
