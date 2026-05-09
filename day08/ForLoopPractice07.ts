4dd1e5458a4aaf3619b9746d0b1ed488b4d3e1f4
ShreyB <shreypractice.github@gmail.com>
2026-05-09 19:44:45 +0530

    Updates_0905_19:45

---
+++ day08/ForLoopPractice07.ts
@@ -0,0 +1,9 @@
+export { }
+console.log("**** Practicing For loop ****");
+
+console.log("\n**** Print Multiples of 5 from 5 to 50 ****");
+
+for(let i:number=5;i<=50;i+=5)
+{
+    console.log(i);
+}
