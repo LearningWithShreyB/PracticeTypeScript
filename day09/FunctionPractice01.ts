41eb366098697bb75aa305a9b074e76fce67e4ca
ShreyB <shreypractice.github@gmail.com>
2026-05-10 16:18:20 +0530

    Updates_1005_16:19

---
+++ day09/FunctionPractice01.ts
@@ -0,0 +1,11 @@
+export { }
+
+console.log("**** Practicing Named Functions ****");
+
+console.log("\n**** Named Function with no parameters and no return type ****");
+
+function f1(): void {
+    console.log("This is a function having no params and no return type");
+}
+
+f1();
