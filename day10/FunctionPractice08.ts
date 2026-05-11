bb8b780cca21bcb4eea869ec693209141d900853
ShreyB <shreypractice.github@gmail.com>
2026-05-11 14:09:57 +0530

    Updates_1105_14:10

---
+++ day10/FunctionPractice08.ts
@@ -0,0 +1,11 @@
+export { }
+
+console.log("**** Practicing Named Functions ****");
+
+console.log("\n**** Arrow Function with No Parameters and No Return Type ****");
+
+let msg = (): void => {
+    console.log("Hello there, Let's learn!!");
+}
+
+msg();
