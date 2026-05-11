b13cbb650b04fd9bc2942115b38ca7d659b72897
ShreyB <shreypractice.github@gmail.com>
2026-05-11 16:10:18 +0530

    Updates_1105_16:11

---
+++ day10/FunctionPractice14.ts
@@ -0,0 +1,11 @@
+export { }
+
+console.log("**** Practicing Named Functions ****");
+
+console.log("\n**** Anonymous function with No Parameters and No Return Type ****");
+
+let greet = function (): void {
+    console.log("Hello there, Keep thrive and learn!!");
+}
+
+greet();
