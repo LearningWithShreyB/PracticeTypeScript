907e16fc840796704cf2f9a419ba30bb84de0dfb
ShreyB <shreypractice.github@gmail.com>
2026-05-12 18:37:49 +0530

    Updates_1205_18:38

--- day11/TuplesPractice03.ts
+++ day11/TuplesPractice03.ts
@@ -2,12 +2,13 @@ export { }
 
 console.log("**** Practicing Tuples ****");
 
-console.log("\n**** Tuple with multiple values ****");
+console.log("\n**** Tuple with multiple values using for in loop ****");
 
 let r2: [number, string, string, string, boolean] = [101, 'SB', '04-1999', 'Graduated', true]
 
 console.log("\n The result of set is:");
 
 for (let index in r2) {
+    console.log(index);
     console.log(r2[index]);
 }
--- day11/TuplesPractice04.ts
+++ day11/TuplesPractice04.ts
@@ -2,13 +2,12 @@ export { }
 
 console.log("**** Practicing Tuples ****");
 
-console.log("\n**** Tuple with multiple values using traditional for loop ****");
+console.log("\n**** Tuple with multiple values using for of loop ****");
 
 let r2: [number, string, string, string, boolean] = [101, 'SB', '04-1999', 'Graduated', true]
 
 console.log("\n The result of set is:");
 
-for (let i: number = 0; i < r2.length; i++) {
-    console.log(i);
-    console.log(r2[i]);
+for (let value of r2) {
+    console.log(value);
 }
