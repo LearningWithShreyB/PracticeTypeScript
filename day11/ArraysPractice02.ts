0f0b7270a040a766045ff68434edef775e84dd3c
ShreyB <shreypractice.github@gmail.com>
2026-05-12 17:21:45 +0530

    Updates_1205_17:22

--- day11/ArraysPractice02.ts
+++ day11/ArraysPractice02.ts
@@ -9,8 +9,14 @@ let empIds: Array<number> = [101, 102, 103, 104]; // Only numbers allowed
 let data: Array<string | number> = ["John", "Smith", 101, 102]; // Union type (strings and numbers)
 let mixedData: Array<any> = [1, "John", true, null]; // Any type allowed (multi-type array)
 
-console.log("\n The result of set is:");
+console.log("\n The result of first set is:");
 
 for (let i: number = 0; i < empIds.length; i++) {
     console.log(empIds[i]);
+}
+
+console.log("\n The result of second set is:");
+
+for (let i: number = 0; i < empNames.length; i++) {
+    console.log(empNames[i]);
 }
