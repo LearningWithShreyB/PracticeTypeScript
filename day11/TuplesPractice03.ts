05403a395c051913d0da68005f2d62e67015cd81
ShreyB <shreypractice.github@gmail.com>
2026-05-12 18:35:20 +0530

    Updates_1205_18:36

---
+++ day11/TuplesPractice03.ts
@@ -0,0 +1,13 @@
+export { }
+
+console.log("**** Practicing Tuples ****");
+
+console.log("\n**** Tuple with multiple values ****");
+
+let r2: [number, string, string, string, boolean] = [101, 'SB', '04-1999', 'Graduated', true]
+
+console.log("\n The result of set is:");
+
+for (let index in r2) {
+    console.log(r2[index]);
+}
