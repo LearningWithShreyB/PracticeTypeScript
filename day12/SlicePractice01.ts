05a60b25d89c3f0e8de1a79582fe154e47cda43b
ShreyB <shreypractice.github@gmail.com>
2026-05-13 23:29:48 +0530

    Updates_1305_23:30

---
+++ day12/SlicePractice01.ts
@@ -0,0 +1,19 @@
+export { }
+
+console.log("**** Practicing Array Methods ****");
+
+console.log("\n**** UnShift method ****");
+
+let fruits: Array<string> = ['apple', 'mango', 'grapes', 'pear', 'melon'];
+
+let numbers: Array<number> = [1,2,3,4,5,6,7,8,9];
+
+console.log("\n The result of first set is:");
+let m1 = numbers.slice(3,5);
+
+console.log("slice element:", m1);
+
+console.log("\n The result of second set is:");
+let m2 = fruits.slice(0,5);
+
+console.log("slice element:", m2);
