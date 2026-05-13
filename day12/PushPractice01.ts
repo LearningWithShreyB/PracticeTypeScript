3fda531320d342144dddc52cb1257b6bb7329b51
ShreyB <shreypractice.github@gmail.com>
2026-05-13 18:12:24 +0530

    Updates_1305_18:13

---
+++ day12/PushPractice01.ts
@@ -0,0 +1,12 @@
+export { }
+
+console.log("**** Practicing Array Methods ****");
+
+console.log("\n**** Push method ****");
+
+let fruits: Array<string> = ['apple', 'mango', 'grapes', 'pear', 'melon'];
+
+console.log("Array before push method:", fruits);
+let m1 = fruits.push('pineapple');
+console.log("Array before push method:", fruits);
+console.log("Push elements:", m1);
