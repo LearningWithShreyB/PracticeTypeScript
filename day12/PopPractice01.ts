f96aaae14ae3ca5c717704b0541c280cbc456228
ShreyB <shreypractice.github@gmail.com>
2026-05-13 18:23:40 +0530

    Updates_1305_18:23

---
+++ day12/PopPractice01.ts
@@ -0,0 +1,12 @@
+export { }
+
+console.log("**** Practicing Array Methods ****");
+
+console.log("\n**** Pop method ****");
+
+let fruits: Array<string> = ['apple', 'mango', 'grapes', 'pear', 'melon'];
+
+console.log("Array before pop method:", fruits);
+let m1 = fruits.pop();
+console.log("Array before pop method:", fruits);
+console.log("pop element:", m1);
