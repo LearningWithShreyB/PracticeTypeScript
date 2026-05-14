a54e34138a2d32c1c0393275a61e240297171b0f
ShreyB <shreypractice.github@gmail.com>
2026-05-14 20:20:05 +0530

    Updates_1405_20:21

--- day13/ArrayMethods04.ts
+++ day13/ArrayMethods04.ts
@@ -24,3 +24,16 @@ let arr02 = fruits.reduce((count,value) => count +1,0)
 
 console.log("Original Array:", fruits);
 console.log("Updated Array:", arr02);
+
+let nums:number[]=[3,4,5,6];
+console.log("\nCount total elements in an array");
+let arr03 = nums.reduce((count,value) => count +1,1)
+
+console.log("Original Array:", nums);
+//if count is 0 -> it will take 0 and then the correct elements
+//if count is 1 or any value -> it will take that value and then from that value it started working
+//For eg count is 2 and original array has 4 elemnts then total count is 2+ original array count
+console.log("Updated Array:", arr03);
+
+
+
