21517f38d5655f087061cdda62ed8c0806fae3d1
ShreyB <shreypractice.github@gmail.com>
2026-05-15 16:04:40 +0530

    Updates_1505_16:05

--- day14/StringMethodsPractice06.ts
+++ day14/StringMethodsPractice06.ts
@@ -6,6 +6,6 @@ console.log("\n**** substring method ****");
 
 let str01:string='Hello TypeScript';
 
-console.log(`Th result of (2,6) is ${str01.substring(2,6)}`);
+console.log(`The result of (2,6) is ${str01.substring(2,6)}`);
 
-console.log(`Th result of (4,13) is ${str01.substring(4,13)}`);
+console.log(`The result of (4,13) is ${str01.substring(4,13)}`);
---
+++ day14/StringMethodsPractice07.ts
@@ -0,0 +1,11 @@
+export { }
+
+console.log("**** Practicing String Methods ****");
+
+console.log("\n**** includes method ****");
+
+let str01:string='Hello TypeScript';
+
+console.log(`Whether 'type' is included?? ${str01.includes('type')}`);
+
+console.log(`Whether 'Script' is included?? ${str01.includes('Script')}`);
