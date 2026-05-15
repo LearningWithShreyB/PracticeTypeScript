343d141d58af4b396ce573fa47764c121d47653d
ShreyB <shreypractice.github@gmail.com>
2026-05-15 17:22:46 +0530

    Updates_1505_17:23

---
+++ day15/Practice01.ts
@@ -0,0 +1,9 @@
+export { }
+
+console.log("\n**** Add an element to the end of an array using push ****");
+
+let numbers:Array<number>=[1,2,3];
+
+numbers.push(4,5);
+
+console.log(`The result is ${numbers}`);
