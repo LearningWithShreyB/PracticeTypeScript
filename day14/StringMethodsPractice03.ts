81c64b150f6bfabcedbac1b03aad30296d363728
ShreyB <shreypractice.github@gmail.com>
2026-05-15 15:52:03 +0530

    Updates_1505_15:52

---
+++ day14/StringMethodsPractice03.ts
@@ -0,0 +1,9 @@
+export { }
+
+console.log("**** Practicing String Methods ****");
+
+console.log("\n**** toLowerCase method ****");
+
+let str01:string='Hello TypeScript';
+
+console.log(`The lower case of the string is ${str01.toLowerCase()}`);
