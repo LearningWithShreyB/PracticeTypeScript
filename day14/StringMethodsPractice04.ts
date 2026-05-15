17e8da46c7248489fc36ff1a9b83e0809de9095a
ShreyB <shreypractice.github@gmail.com>
2026-05-15 15:56:05 +0530

    Updates_1505_15:57

---
+++ day14/StringMethodsPractice04.ts
@@ -0,0 +1,11 @@
+export { }
+
+console.log("**** Practicing String Methods ****");
+
+console.log("\n**** charAt method ****");
+
+let str01:string='Hello TypeScript';
+
+console.log(`Character at 2th index is ${str01.charAt(2)}`);
+
+console.log(`Character at 10th index is ${str01.charAt(10)}`);
