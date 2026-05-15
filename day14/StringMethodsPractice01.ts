bca6538a57afb4700ed1c287c1e50bdf5ee1ffed
ShreyB <shreypractice.github@gmail.com>
2026-05-15 15:49:47 +0530

    Updates_1505_15:50

---
+++ day14/StringMethodsPractice01.ts
@@ -0,0 +1,9 @@
+export { }
+
+console.log("**** Practicing String Methods ****");
+
+console.log("\n**** length method ****");
+
+let str01:string='Hello TypeScript';
+
+console.log(`The length of the string is ${str01.length}`);
