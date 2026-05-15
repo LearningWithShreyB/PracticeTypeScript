1ea671721bb2d0d21b9289ce1ccb5e37efa94667
ShreyB <shreypractice.github@gmail.com>
2026-05-15 17:34:41 +0530

    Updates_1505_17:35

--- day15/Practice01.ts
+++ day15/Practice01.ts
@@ -2,7 +2,7 @@ export { }
 
 console.log("\n**** Add an element to the end of an array using push ****");
 
-let numbers:Array<number>=[1,2,3];
+let numbers:Array<number|string>=[1,2,3];
 
 numbers.push(4,5);
 
@@ -22,8 +22,18 @@ numbers.shift();
 
 console.log(`The result is ${numbers}`);
 
-console.log("\n**** Add elements at the beginning of an array using unshif ****");
+
+console.log("\n**** Add elements at the beginning of an array using unshift ****");
 
 numbers.unshift(1);
 
 console.log(`The result is ${numbers}`);
+
+
+console.log("\n**** Concatenate two arrays using concat ****");
+
+let fruits:(number|string)[]=['apple','mango']
+
+let total:(number|string)[]=numbers.concat(fruits);
+
+console.log(`The result is ${total}`);
