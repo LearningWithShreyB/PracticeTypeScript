1583c70e07d20e6497d694908407ac8714e82572
ShreyB <shreypractice.github@gmail.com>
2026-05-15 15:51:18 +0530

    Updates_1505_15:52

---
+++ day14/StringMethodsPractice02.ts
@@ -0,0 +1,9 @@
+export { }
+
+console.log("**** Practicing String Methods ****");
+
+console.log("\n**** toUpperCase method ****");
+
+let str01:string='Hello TypeScript';
+
+console.log(`The upper case of the string is ${str01.toUpperCase()}`);
