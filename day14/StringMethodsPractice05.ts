ed8e4eba57528c0a631425883947d592ab1bff98
ShreyB <shreypractice.github@gmail.com>
2026-05-15 15:58:24 +0530

    Updates_1505_15:59

---
+++ day14/StringMethodsPractice05.ts
@@ -0,0 +1,9 @@
+export { }
+
+console.log("**** Practicing String Methods ****");
+
+console.log("\n**** indexOf method ****");
+
+let str01:string='Hello TypeScript';
+
+console.log(`Index of Script is ${str01.indexOf('Script')}`);
