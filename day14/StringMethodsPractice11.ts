40cb172718f42862ace6ae3bebdc6408dcadc5fb
ShreyB <shreypractice.github@gmail.com>
2026-05-15 17:01:46 +0530

    Updates_1505_17:02

---
+++ day14/StringMethodsPractice11.ts
@@ -0,0 +1,12 @@
+export { }
+
+console.log("**** Practicing String Methods ****");
+
+console.log("\n**** trim, trimStart, trimEnd method ****");
+
+let str01:string='    Hello     TypeScript    ';
+
+console.log(`The result is ****${str01}****`);
+console.log(`The result is ****${str01.trim()}****`);
+console.log(`The result is ****${str01.trimStart()}****`);
+console.log(`The result is ****${str01.trimEnd()}****`);
