7c5b71046f98bfcfbb6f49f4fb9e4d2c82a3482e
ShreyB <shreypractice.github@gmail.com>
2026-05-15 17:09:30 +0530

    Updates_1505_17:10

--- day14/StringMethodsPractice12.ts
+++ day14/StringMethodsPractice12.ts
@@ -4,9 +4,9 @@ console.log("**** Practicing String Methods ****");
 
 console.log("\n**** concat method ****");
 
-str1="welcome ";
-str2="to typescript";
-str3=" and javascript";
+let str1:string="welcome ";
+let str2:string="to typescript";
+let str3:string=" and javascript";
 
 console.log(`The result is ${str1.concat(str2)}`);
 console.log(`The result is ${str1.concat(str2).concat(str3)}`);
