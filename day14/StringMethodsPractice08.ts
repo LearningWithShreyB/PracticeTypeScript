dea8b118e9bd521bfb7fe7028ac3c69d43d96e86
ShreyB <shreypractice.github@gmail.com>
2026-05-15 16:35:39 +0530

    Updates_1505_16:36

---
+++ day14/StringMethodsPractice08.ts
@@ -0,0 +1,15 @@
+export { }
+
+console.log("**** Practicing String Methods ****");
+
+console.log("\n**** startsWith & endsWith method ****");
+
+let str01:string='Hello TypeScript';
+
+console.log(`The result is ${str01.startsWith('type')}`);
+
+console.log(`The result is ${str01.startsWith('He')}`);
+
+console.log(`The result is ${str01.endsWith('Script')}`);
+
+console.log(`The result is ${str01.endsWith('ri')}`);
