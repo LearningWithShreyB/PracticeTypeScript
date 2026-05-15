7b6851b27928cc7b41154e966406ed569eb6abff
ShreyB <shreypractice.github@gmail.com>
2026-05-15 16:41:08 +0530

    Updates_1505_16:42

---
+++ day14/StringMethodsPractice09.ts
@@ -0,0 +1,12 @@
+export { }
+
+console.log("**** Practicing String Methods ****");
+
+console.log("\n**** replace method ****");
+
+let str01:string='Hello TypeScript';
+
+console.log(`The result is ${str01.replace('Type','Java')}`);
+
+console.log(`The result is ${str01.replace('TypeScript','there!!')}`);
+
