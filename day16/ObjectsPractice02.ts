db269902e3ca2bdea00606287689b07e94c00407
ShreyB <shreypractice.github@gmail.com>
2026-05-16 11:22:43 +0530

    Updates_1605_11:22

---
+++ day16/ObjectsPractice02.ts
@@ -0,0 +1,25 @@
+export{}
+
+console.log("**** Practicing Object in TypeScript ****");
+
+console.log("\n**** Using 'object' type ****");
+
+let employee =
+{
+    name:"Shrey",
+    eid:101,
+    salary:50000,
+    job:'Sales Executive',
+    getDetails:function():string
+    {
+        return `${this.name} is working as ${this.job} having ${this.eid} and earning ${this.salary}`;
+    }
+}
+
+//Approach 1
+
+console.log(`The name of the employee is ${employee["name"]}`);
+console.log(`The employee ID of the employee is ${employee["eid"]}`);
+console.log(`The salary of the employee is ${employee["salary"]}`);
+console.log(`The job of the employee is ${employee["job"]}`);
+console.log(`The overall details of the employee is ${employee["getDetails"]()}`);
