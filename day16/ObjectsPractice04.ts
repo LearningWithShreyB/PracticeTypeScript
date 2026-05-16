3c8f12427cfbbc430b0502e02b8aeeec5e9d1b60
ShreyB <shreypractice.github@gmail.com>
2026-05-16 13:36:43 +0530

    Updates_1605_13:37

---
+++ day16/ObjectsPractice04.ts
@@ -0,0 +1,24 @@
+export { }
+
+console.log("**** Practicing Object in TypeScript ****");
+
+console.log("\n**** Inline Type Object ****");
+
+let student: {
+    name: string,
+    age: number,
+    grade: string,
+    getSummary: () => string
+} = {
+    name: "Scott",
+    age: 15,
+    grade: "A",
+    getSummary: () => "" // temporary placeholder
+};
+
+// Now assign the actual arrow function
+student.getSummary = () => {
+    return `${student.name} is ${student.age} years old and scored grade ${student.grade}`;
+};
+
+console.log(student.getSummary());
