38f474ffb3dfaa478f498c6f75941ffc4266635e
ShreyB <shreypractice.github@gmail.com>
2026-05-18 23:48:28 +0530

    Updates_1805_23:49

---
+++ day19/ModulePractice02.ts
@@ -0,0 +1,11 @@
+console.log("**** Practicing Module Concept in TypeScript ****");
+
+console.log("\n**** import method ****");
+
+import * as utils from "./ModulePractice01"
+
+console.log(`The variable value is ${utils.a}`);
+
+console.log(utils.m1());
+
+console.log(`The class implementation is ${utils.Length.len("Bye SB!!")}`);
